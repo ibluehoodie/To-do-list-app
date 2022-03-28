a0d404f6d6f5b5e91670a3fa843571d562ebed22
ibluehoodie <42532938+ibluehoodie@users.noreply.github.com>
2022-03-28 18:06:44 -0400

    Part2 rewrite crossOut function using jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ function newItem(){
 
 //1. Adding a new item to the list of items:
    // let li = document.createElement("li");
-   let li = $('<li></li>'');
+   let li = $('<li></li>');
    // let inputValue = document.getElementById("input").value;
    let inputValue = $('#input').val();
    // let text = document.createTextNode(inputValue);
@@ -13,7 +13,7 @@ function newItem(){
    li.append(inputValue);
 
    if (inputValue === '') {
-     alert("You must write something!");
+     alert('You must write something!');
    } else {
      // let list = document.querySelector('#list');
      // list.appendChild(li);
@@ -25,10 +25,12 @@ function newItem(){
 
  //2. Crossing out an item from the list of items:
    function crossOut() {
- 		li.classList.toggle("strike");
+ 		// li.classList.toggle("strike");
+    li.toggleClass('strike');
  	}
 
- 	li.addEventListener("dblclick",crossOut);
+ 	// li.addEventListener('dblclick', crossOut);
+  li.on('dblclick', crossOut);
 
  //3(i). Adding the delete button "X":
    let crossOutButton = document.createElement("crossOutButton");
